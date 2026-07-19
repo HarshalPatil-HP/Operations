33a38f90e1aa2f2f5d83f00ce292141ad91cff60
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-07-19 23:44:19 +0530

    Clean up index.js and add debugging log
    Removed unnecessary blank lines and added a debugging log.

--- index.js
+++ index.js
@@ -13,16 +13,11 @@ app.post('/teas',(req,res)=>{
     tea.push(newtea);
     res.status(200).send(newtea);
 })
-//render data
 app.get('/teas',(req,res)=>{
   res.status(200).send(tea);
 console.log("checking")
 })
-
-
-
-
-
+console.log("for debugging");
 app.listen(port ,()=>{
     console.log("iam listning move .")
 })
