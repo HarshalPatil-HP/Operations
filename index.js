cb6f068a1d7b4e93b29e1f14eb1b17c7d78f2690
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-07-19 23:43:31 +0530

    Fix console log message and remove comment

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ let port = 3000
 let id=1;
 let tea=[]
 
-//tea leliya
 app.post('/teas',(req,res)=>{
     const {name,price}=req.body;
     const newtea={id:id++,name,price};
@@ -25,5 +24,5 @@ console.log("checking")
 
 
 app.listen(port ,()=>{
-    console.log("iam listning move on.........")
+    console.log("iam listning move .")
 })
