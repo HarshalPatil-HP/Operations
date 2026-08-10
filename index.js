41c8458e3e59b06a863f9734b704dc6dc50985a5
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-08-10 22:23:52 +0530

    Add POST endpoint to create new tea entries

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app=express();
 let port = 3000;
 let id=1;
 let tea=[];
+
 app.post('/teas',(req,res)=>{
     const {name,price}=req.body;
     const newtea={id:id++,name,price};
     tea.push(newtea);
     res.status(200).send(newtea);
-})
+});
 app.get('/teas',(req,res)=>{
   res.status(200).send(tea);
 console.log("checking");
