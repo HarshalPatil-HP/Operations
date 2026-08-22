04bf5b6c065c59430c4ae261908245c70b44bb7e
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-08-22 19:41:40 +0530

    Add POST and GET endpoints for tea management

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import express from 'express';
 const app=express();
+
 let port = 3000;
 let id=1;
 let tea=[];
+
 app.post('/teas',(req,res)=>{
     const {name,price}=req.body;
     const newtea={id:id++,name,price};
     tea.push(newtea);
     res.status(200).send(newtea);
 });
+
 app.get('/teas',(req,res)=>{
   res.status(200).send(tea);
 console.log("checking");
 });
+
 app.listen(port ,()=>{
     console.log("iam listning move .");
 });
